bb51b7eba1da0d8b95afbe213dacc6cabafe90f3
stonasss <luis.b.santos96@gmail.com>
2023-01-13 16:58:40 -0300

    servidor abrindo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb";
 
-const mongoClient = new MongoClient("localhost://127.0.0.1:27017")
+const PORT = 5000;
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+let db;
 
-mongoClient.connect().then().catch()
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db("batepapo-uol");
+    console.log("Conectado ao mongodb!");
+  })
+  .catch(() => {
+    console.log("Erro na conexão");
+  });
 
 const server = express();
 server.use(express.json());
-server.use(cors())
+server.use(cors());
+
+server.get("/participants", (req, res) => {
+  const participants = db
+    .collection("participants")
+    .find()
+    .toArray()
+    .then((dados) => {
+      return res.send(dados);
+    })
+    .catch(() => {
+      res.status(500).send("Problema no servidor de banco de dados");
+    });
+});
+
+server.listen(PORT, () => {
+  console.log("Servidor aberto!");
+});
