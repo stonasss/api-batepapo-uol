2c55c3e5dbdeff097679c7d4ed8b0e472955d6c5
stonasss <luis.b.santos96@gmail.com>
2023-01-14 08:37:11 -0300

    ajustes ao get de messages & participants + post participants & status code

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get("/participants", (req, res) => {
-  const participants = db
+server.get("/participants", async (_, res) => {
+  const participants = await db
     .collection("participants")
     .find()
     .toArray()
@@ -26,28 +26,68 @@ server.get("/participants", (req, res) => {
     .catch(() => {
       res.status(500).send("Problema no servidor de banco de dados");
     });
-    return res.status(201).send("Participantes obtidos")
+  return res.status(201).send(participants);
 });
 
-server.get("/messages", (req, res) => {
-  console.log(req.query);
+server.get("/messages", async (req, res) => {
+  const { user } = req.headers;
+  const { query } = req;
   const { limit } = req.query;
+  const messages = await db.collection("messages").find().toArray();
+  let userMsgs = messages.filter(
+    (msg) =>
+      msg.user === user ||
+      msg.to === "Todos" ||
+      msg.from === user ||
+      msg.to === user ||
+      msg.type === "status"
+  );
 
-  const messages = db
-    .collection("messages")
-    .find()
-    .toArray()
-    .then((dados) => {
-      const recentMsgs = dados.reverse().slice(0, parseInt(limit));
-      return res.send(recentMsgs);
-    });
+  try {
+    if (query && limit && isNaN(Number(limit) || Number(limit) < 1)) {
+      return res.status(422).send("Query inválido");
+    }
+    if (limit) {
+      return res.status(201).send(userMsgs.slice(-limit));
+    }
+    return res.status(201).send(userMsgs.reverse());
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 server.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
+  const user = req.headers.User;
+  const date = dayjs(Date.now()).format("hh:mm:ss");
 
-  if (type !== "private_message" || type !== "message")
-    return res.status(422).send("Tipo de mensagem inválido");
+  const msgSchema = Joi.object({
+    to: Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().valid("private_message", "message").required(),
+  });
+  const validMsg = msgSchema.validate(
+    { to, text, type },
+    { abortEarly: false }
+  );
+  if (validMsg.error) return res.status(422).send(validMsg.error.details);
+
+  if (to === "" || type === "")
+    return res.status(422).send("Preencha os campos vazios");
+
+  try {
+    const Anon = await db.collection("messages").findOne({ user });
+    if (!Anon) return res.status(422).send("User inexistente");
+    await db.collection("messages").insertOne({
+      from: user,
+      to: to,
+      text: text,
+      type: type,
+      time: date,
+    });
+  } catch {
+    res.status(422).send("Mensagem inválida");
+  }
 });
 
 server.post("/participants", async (req, res) => {
@@ -56,10 +96,10 @@ server.post("/participants", async (req, res) => {
   const lastStatus = Date.now();
 
   const userSchema = Joi.object({
-    name: Joi.string().required()
-  })
-  const validUser = userSchema.validate({ name }, { abortEarly: false })
-  if (validUser.error) return res.status(422).send("Participante inválido")
+    name: Joi.string().required(),
+  });
+  const validUser = userSchema.validate({ name }, { abortEarly: false });
+  if (validUser.error) return res.status(422).send("Participante inválido");
 
   if (name === "") return res.status(422).send("Preencha o campo vazio");
 
@@ -72,8 +112,8 @@ server.post("/participants", async (req, res) => {
       to: "Todos",
       text: "entra na sala...",
       type: "status",
-      time: date
-    })
+      time: date,
+    });
   } catch (err) {
     console.log(err);
     res.status(422).send("Participante não registrado");
